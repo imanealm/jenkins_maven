45454312db16426eb366e04cc0f9bce336d9b03e
PC i5 <iaitlmaalem@gmail.com>
2020-01-07 21:17:10 +0100

    heloo

--- eclipse-workspace/devops2/src/devops2/helloworld.java
+++ eclipse-workspace/devops2/src/devops2/helloworld.java
@@ -2,5 +2,11 @@ package devops2;
 
 public class helloworld
 {
+	public static void main( String[] args )
+    {
+		System.out.println( "Hello World!" );
+		System.out.println( "Master" );  
 
 }
+}
+
