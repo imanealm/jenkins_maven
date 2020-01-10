1d0d57d37fb80e7f974948e15cae9a3eb8b7a451
PC i5 <iaitlmaalem@gmail.com>
2020-01-10 22:22:44 +0100

    jenkins

--- eclipse-workspace/devops2/src/devops2/helloworld.java
+++ eclipse-workspace/devops2/src/devops2/helloworld.java
@@ -4,8 +4,8 @@ public class helloworld
 {
 	public static void main( String[] args )
     {
-		System.out.println( "Hello World!" );
-		System.out.println( "Master" );  
+		System.out.println( "Hello World!2" );
+		System.out.println( "Master2" );  
 
 }
 }
